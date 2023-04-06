0776062a3f895ccaef2d71cd45c5d68aa68e62ec
d-siden <111708784+d-siden@users.noreply.github.com>
2023-04-06 11:58:42 -0300

    separacao por codigo
    Evita misturar estacoes caso o nome seja igual. Dessa vez junta o nome e o codigo para diferenciar uma estacao.

--- tratar_cemaden.R
+++ tratar_cemaden.R
@@ -1,13 +1,13 @@
 ORDENAR_CEMADEN <- function(endereco, horario=F){
   # carregar arquivo
   mes <- read.csv(file = endereco,
-                   sep = ";",
-                   dec = ",")
-
+                  sep = ";",
+                  dec = ",")
+  
   # essa seria a saida - terminou não sendo
   # colocar somente as colunas que interessam
   mensal <- data.frame("data" = mes$datahora,
-                       "nome" = mes$nomeEstacao,
+                       "nome" = paste(mes$nomeEstacao, mes$codEstacao, sep = "_"),
                        "lat" = mes$latitude,
                        "lon" = mes$longitude,
                        "chuva" = mes$valorMedida)
@@ -22,14 +22,14 @@ ORDENAR_CEMADEN <- function(endereco, horario=F){
     if(horario==FALSE){
       # remover a hora das datas
       mensal1$data <- substr(mensal1$data, 1, 10)
-    
+      
       # TRANSFORMANDO EM FACTOR!!!!!!!!!!!!!!
       # I'M A MOTHERFUCKING GENIOUS!!
       mensal1$data <- as.factor(mensal1$data)
       
       # criar dataframe para receber os valores por data
       mensal2 <- data.frame()
-    
+      
       # dataframe para receber os valores diarios prontos somados
       diarios <- data.frame()
       # resetar esse dataframe também separa os arquivos pelo fator 'nome'
@@ -51,31 +51,31 @@ ORDENAR_CEMADEN <- function(endereco, horario=F){
       
       # converter chuva em numeric
       diarios$chuva <- as.numeric(diarios$chuva)
-  
+      
       # ja pode fechar o mes e salvar
       # salvar no endereço de entrada
       write.csv2(diarios,
-                file = sprintf("%s-%s-diario.csv", fatornome, substr(fatordata, 1, 7)),
-                row.names = F,
-                )
+                 file = sprintf("%s-%s-diario.csv", fatornome, substr(fatordata, 1, 7)),
+                 row.names = F,
+      )
     }else{
       #ou transformar em horario:
-
+      
       # resumir a coluna data para apenas dia e hora
       for(dth in 1:nrow(mensal1)){
         mensal1$data[dth] <- strsplit(mensal1$data, split=":")[[dth]][1]
       }
-
+      
       # transformar em factor      
       mensal1$data <- as.factor(mensal1$data)
       
       # criar dataframe para receber os valores por data e hora
       mensal2 <- data.frame()
-
+      
       # dataframe para receber os valores horarios prontos somados
       horarios <- data.frame()
       # resetar esse dataframe tambem separa os arquivos pelo fator 'nome'
-
+      
       # separar por fator-datahora
       for (fatordatahora in levels(mensal1$data)){
         mensal2 <- mensal1[mensal1$data == fatordatahora, ]
@@ -93,13 +93,13 @@ ORDENAR_CEMADEN <- function(endereco, horario=F){
       
       # converter chuva em numeric
       horarios$chuva <- as.numeric(horarios$chuva)
-  
+      
       # ja pode fechar o mes e salvar
       # salvar no endereço de entrada
       write.csv2(horarios,
-                file = sprintf("%s-horario.csv", fatornome),
-                row.names = F,
-                )
+                 file = sprintf("%s-horario.csv", fatornome),
+                 row.names = F,
+      )
     }  # horario
   }  # separou por nome
   cat("Arquivos salvos em:", getwd())
